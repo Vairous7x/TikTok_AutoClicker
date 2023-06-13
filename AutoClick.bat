a374c31f16ed743a7153611dc519e772c414f4b2
Vairous 7x <46539342+Vairous7x@users.noreply.github.com>
2023-06-13 20:59:09 +0300

    Update AutoClick.bat

--- AutoClick.bat
+++ AutoClick.bat
@@ -1,1 +1,12 @@
-
+@echo off
+cls
+color 4
+adb shell "input tap 570 770 & sleep 0.1; input tap 570 770"
+adb shell "input tap 570 780 & sleep 0.1; input tap 570 780"
+adb shell "input tap 570 790 & sleep 0.1; input tap 570 790"
+adb shell "input tap 570 800 & sleep 0.1; input tap 570 800"
+adb shell "input tap 570 810 & sleep 0.1; input tap 570 810"
+adb shell "input tap 570 820 & sleep 0.1; input tap 570 820"
+adb shell "input tap 570 830 & sleep 0.1; input tap 570 830"
+adb shell "input tap 570 840 & sleep 0.1; input tap 570 840"
+adb shell "input tap 570 850 & sleep 0.1; input tap 570 850"
